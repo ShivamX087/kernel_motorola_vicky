180ff21c6064d1bd5d4efa1ad4560d314dcfb034
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:24:00 -0800

    devfreq: Introduce devfreq boost driver
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpu-llcc-ddr-bw
    devfreq device, but the driver is structured in a way that makes it
    easy to add support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -189,6 +189,10 @@ struct devfreq {
 	struct dev_pm_qos_request user_max_freq_req;
 	unsigned long scaling_min_freq;
 	unsigned long scaling_max_freq;
+	unsigned long min_freq;
+	unsigned long max_freq;
+	bool is_boost_device;
+	bool max_boost;
 	bool stop_polling;
 
 	unsigned long suspend_freq;
